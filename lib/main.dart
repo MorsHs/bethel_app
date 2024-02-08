30f0df7e495beb03c69f2beed968bfb0e89739ac
MorsHs <skyblade163@gmail.com>
2024-02-08 17:12:12 +0800

    Rewrite and Implemented FutureBuilder (buggy)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 
-import 'package:bethel_app_final/controller/user_location.dart';
-import 'package:bethel_app_final/screens/map_page.dart';
 import 'package:flutter/material.dart';
+
+import 'pages/map_page.dart';
 
 void main() {
   runApp(const MyApp());
